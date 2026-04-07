cbdb969273d7c5869f251ad85f4112a3940ab754
Filip Iliev <151552696+grga1@users.noreply.github.com>
2026-04-07 22:56:30 +0200

    Add ER diagram and DML query requirement in 2.java
    Added a Java file containing an ER diagram description and a DML query requirement for musicians playing guitar at concerts.

---
+++ sql/2.java
@@ -0,0 +1,18 @@
+На сликата подолу е прикажан дел од ЕР дијаграм за систем за организација на музички фестивали и концерти.
+Релационата база е дефинирана преку следните релации: 
+Muzicar(id, ime, prezime, datum_ragjanje)
+Muzicar_instrument(id_muzicar*, instrument)
+Bend(id, ime, godina_osnovanje)
+Bend_zanr(id_bend*, zanr)
+Nastan(id, cena, kapacitet)
+Koncert(id*, datum, vreme)
+Festival(id*, ime)
+Festival_odrzuvanje(id*, datum_od, datum_do)
+Muzicar_bend(id_muzicar*, id_bend*, datum_napustanje)
+Festival_bend(id_festival*, datum_od*, id_bend*)
+Koncert_muzicar_bend(id_koncert*, id_muzicar*, id_bend*)
+
+Да се напише DML израз со кој ќе се вратат имињата и презимињата на гитаристите 
+(музичарите кои свират на инструментот гитара) кои настапиле на концерт заедно со бенд откако го напуштиле.
+Датумот на настап на музичарот заедно со бендот е датумот на самиот концерт. 
+Резултатите треба да се подредени според името во растечки редослед. 
